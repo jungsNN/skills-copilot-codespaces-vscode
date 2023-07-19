3a4911af98b272a57d3f5944fbd3dedf248d2979
Jenny Jung  ☻ <60561405+jungsNN@users.noreply.github.com>
2023-07-19 18:47:05 +0000

    update skillsMember fn

--- member.js
+++ member.js
@@ -1,28 +1,10 @@
-function skillsMembers() {
-  return {
-    restrict: 'E',
-    templateUrl: 'templates/directives/skills-members.html',
-    scope: {
-      members: '=',
-      skills: '='
-    },
-    controller: function ($scope, $state, $stateParams, $ionicModal, $ionicPopup, $ionicLoading, $ionicScrollDelegate, $timeout, $ionicHistory, $translate, $rootScope, $ionicPopover, $ionicPlatform, $ionicActionSheet, $ionicBackdrop, $ionicSlideBoxDelegate, $ionicSideMenuDelegate, $ionicViewSwitcher, $ionicViewService, $ionicTabsDelegate, $ionicNavBarDelegate, $ionicListDelegate, $ionicSlideBoxDelegate, $ionicScrollDelegate, $ionicPopover, $ionicHistory, $ionicSlideBoxDelegate, $ionicLoading, $ionicModal, $ionicPopup, $ionicActionSheet, $ionicBackdrop, $ionicSideMenuDelegate, $ionicViewSwitcher, $ionicViewService, $ionicTabsDelegate, $ionicNavBarDelegate, $ionicListDelegate, $ionicScrollDelegate, $ionicPopover, $ionicHistory) {
-
-      $scope.showModal = function (id) {
-        $ionicModal.fromTemplateUrl('templates/modal-skills.html', {
-          scope: $scope,
-          animation: 'slide-in-up'
-        }).then(function (modal) {
-          $scope.modal = modal;
-          $scope.modal.show();
-        });
-      };
-
-      $scope.closeModal = function () {
-        $scope.modal.hide();
-        $scope.modal.remove();
-      };
-
-    }
-  };
+function skillsMember()
+{
+    var skills = ["C#", "ASP.NET", "SQL Server", "HTML", "CSS"];
+    var member = {
+        name: "John Doe",
+        age: 34,
+        skills: skills
+    };
+    return member;
 }
